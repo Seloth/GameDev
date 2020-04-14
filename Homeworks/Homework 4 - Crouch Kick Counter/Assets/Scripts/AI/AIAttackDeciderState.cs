5c0ba4e8438d97d7fc39673438e7e62e64397301
Seloth <37912222+Seloth@users.noreply.github.com>
2020-04-14 20:28:04 +0300

    homework 4 fix 2

--- Homeworks/Homework 4 - Crouch Kick Counter/Assets/Scripts/AI/AIAttackDeciderState.cs
+++ Homeworks/Homework 4 - Crouch Kick Counter/Assets/Scripts/AI/AIAttackDeciderState.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
 
 public class AIAttackDeciderState : StateMachineBehaviour {
+
+	[SerializeField]
+	[Range(0.1f, 0.4f)]
+	private float wantedDistanceToPlayer = 0.4f;
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		float rand = Random.value;
-		if      (rand <= 0.3f) { animator.SetTrigger("ShouldJump"); }
-		else if (rand <= 0.6f) { animator.SetTrigger("ShouldMoveToPlayer"); }
-		else if (rand <= 1.0f) { animator.SetTrigger("ShouldCrouch"); }
+
+		GameObject player = GameObject.FindWithTag("Player");
+		Transform playerTransform = player.transform;
+
+		Vector3 vectorToPlayer = playerTransform.position - animator.transform.position;
+		float distanceToPlayer = vectorToPlayer.magnitude;
+
+		if (distanceToPlayer > wantedDistanceToPlayer )
+		{
+			animator.SetTrigger("ShouldMoveToPlayer");
+		}
+		else
+		{
+			float rand = Random.value;
+			if (rand <= 0.3f) { animator.SetTrigger("ShouldJump"); }
+			else if (rand <= 0.6f) { animator.SetTrigger("ShouldMoveToPlayer"); }
+			else if (rand <= 1.0f) { animator.SetTrigger("ShouldCrouch"); }
+		}
+		
 	}
 }
--- Homeworks/Homework 4 - Crouch Kick Counter/Assets/Scripts/AI/AIDeciderState.cs
+++ Homeworks/Homework 4 - Crouch Kick Counter/Assets/Scripts/AI/AIDeciderState.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
 
 public class AIDeciderState : StateMachineBehaviour {
+
+	[SerializeField]
+	[Range(0.1f, 0.4f)]
+	private float blockDistanceToPlayer = 0.2f;
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		float rand = Random.value;
-		if      (rand <= 0.2f) { animator.SetBool("ShouldRetreat", true); }
-		else if (rand <= 0.4f) { animator.SetTrigger("ShouldWait"); }
-		else if (rand <= 1.0f) { animator.SetTrigger("ShouldAttack"); }
+
+		GameObject player = GameObject.FindWithTag("Player");
+		Animator playerAnimator = player.GetComponent<Animator>();
+		Transform playerTransform = player.transform;
+
+		Vector3 vectorToPlayer = playerTransform.position - animator.transform.position;
+		float distanceToPlayer = vectorToPlayer.magnitude;
+
+		if (distanceToPlayer < blockDistanceToPlayer /*&& playerAnimator.GetBool("IsPunching")*/)
+		{
+			animator.SetTrigger("ShouldCrouch");
+		}
+		else
+		{
+			float rand = Random.value;
+			if (rand <= 0.2f) { animator.SetBool("ShouldRetreat", true); }
+			else if (rand <= 0.4f) { animator.SetTrigger("ShouldWait"); }
+			else if (rand <= 1.0f) { animator.SetTrigger("ShouldAttack"); }
+		}
 	}
 }
